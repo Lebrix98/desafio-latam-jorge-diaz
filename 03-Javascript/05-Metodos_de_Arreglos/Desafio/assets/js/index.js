6f4c9c34152b631cd7380b0a1d78d0ee5a6d5250
Jorge Diaz <jorgediazl1998@gmail.com>
2024-11-12 00:30:59 -0300

    Challenge advance

--- 03-Javascript/05-Metodos_de_Arreglos/Desafio/assets/js/index.js
+++ 03-Javascript/05-Metodos_de_Arreglos/Desafio/assets/js/index.js
@@ -10,7 +10,12 @@ const listTodo = document.querySelector("#todoList");
 
 // Array vacio
 let array = [];
+let valTotal = 0;
+let valRealize = 0;
 let i = 0;
+
+total.innerHTML = valTotal;
+realizadas.innerHTML = valRealize;
 
 const renderTodo = () => {
   let printHtml = "";
@@ -19,11 +24,12 @@ const renderTodo = () => {
       <tr>
       <td>${item.id}</td>
       <td>${item.todo}</td>
-      <td><input type="checkbox" checked="${item.check}" /></td>
+      <td><input type="checkbox" onclick="completeTodo(${item.id})" ${
+      item.check === true ? `checked` : ``
+    } /></td>
       <td><button onclick="deleteTodo(${item.id})">X</button></td>
       </tr>`;
   }
-
   listTodo.innerHTML = printHtml;
 };
 
@@ -31,10 +37,10 @@ function addTodo() {
   let val = { id: i, todo: inputText.value, check: false };
 
   if (val.todo.length > 0) {
-    val.id = i++
+    val.id = i++;
     array.push(val);
     inputText.value = "";
-
+    updateValue();
     renderTodo();
   }
 }
@@ -42,6 +48,18 @@ function addTodo() {
 const deleteTodo = (id) => {
   let index = array.findIndex((elem) => elem.id == id);
   array.splice(index, 1);
-
+  updateValue();
   renderTodo();
+};
+
+const completeTodo = (id) => {
+  const todoComplete = array.find((elem) => elem.id === id);
+  todoComplete.check = true;
+  updateValue();
+};
+
+const updateValue = () => {
+  let countTodos = array.filter((elem) => elem.check === true);
+  total.innerHTML = array.length;
+  realizadas.innerHTML = countTodos.length;
 };
